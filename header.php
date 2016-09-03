cf79f203cfb1a803d8273e684c0b1f1c2d066a43
David Truxall <davetrux@gmail.com>
2016-09-02 21:47:29 -0400

    Added analytics support

--- header.php
+++ header.php
@@ -22,6 +22,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php include_once("inc/analyticstracking.php") ?>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'materialwp' ); ?></a>
 
